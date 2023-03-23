94160672299b3bab3860d36adad1e1a4a3954d08
iastac84 <istacey@outlook.com>
2023-03-23 18:16:19 +0000

    100daysofpython day 2

---
+++ 02-helloworld-with-variable.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+
+# Hello world with simple variable
+message = "Hello Python world!" 
+print(message) 
+
+# Changing the variable
+message = "This is 100 days of Python!" 
+print(message) 
