eb818856ea4d41f452d764d5acf3fdc86debcc2f
iastac84 <istacey@outlook.com>
2023-03-25 21:05:41 +0000

    100daysofpython day 4

---
+++ 04-cancat-str-and-int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+
+# Add to integers, then concatenate strings and integers with +
+integer1 = 44
+integer2 = 22
+total = integer1 + integer2
+
+print ("The sum of " + str (integer1) + " and " + str (integer2) + " is " + str (total))  
