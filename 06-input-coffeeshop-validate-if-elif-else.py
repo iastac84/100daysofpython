4e050b7260e08f672060e12231092dc72e5788ca
iastac84 <istacey@outlook.com>
2023-03-27 14:16:48 +0100

    100daysofpython

--- 06-input-coffeeshop-validate-if-elif-else.py
+++ 06-input-coffeeshop-validate-if-elif-else.py
@@ -6,37 +6,37 @@
 # Building on 05-input-coffeeshop.py we verify the customer's choice and price the items differently
 
 # Variables
-COFFEESHOPNAME = "iastac84"
+COFFEESHOPNAME= "iastac84"
+MENU = "Black Coffee @5.15, Espresso @4.99, Latte @5.55 and Cappucino @6.25"
+
 print ("Hello, welcome to " + COFFEESHOPNAME + " Coffee!!!")
 
-# Prompt user for input
+# Prompt user for input and return with Proper Case with title()
 NAME = input ("What is your name?\n")
-
-print ("Hello " + NAME + " thank you for coming in today!\n\n")
-
-MENU = "Black Coffee @5.15, Espresso @4.95, Latte @5.55 and Cappucino @6.25"
+print ("Hello " + NAME.title() + " thank you for coming in today!\n\n")
 
 # Prompt user for input again (with verification of user input this time)
-ORDER = input (NAME + ", what would you like from our menu today? We have....\n" + MENU + "\n")
+# Using .lower() so the user/client can use any capitalisation and we still match, with the output returned in Proper Case
+ORDER = input (NAME.title() + ", what would you like from our menu today? We have....\n" + MENU + "\n")
 
 if ORDER.lower() == "black coffee":
-    QTY = input ("Sounds good " + NAME + ", how many cups of " + ORDER + " would you like?\n")
+    QTY = input ("Sounds good " + NAME.title() + ", how many cups of " + ORDER.title() + " would you like?\n")
     PRICE = 5.15
     print ("")
 elif ORDER.lower() == "espresso":
-    QTY = input ("Sounds good " + NAME + ", how many cups of " + ORDER + " would you like?\n")
-    PRICE = 4.95
+    QTY = input ("Sounds good " + NAME.title() + ", how many cups of " + ORDER.title() + " would you like?\n")
+    PRICE = 4.99
     print ("")
 elif ORDER.lower() == "latte":
-    QTY = input ("Sounds good " + NAME + ", how many cups of " + ORDER + " would you like?\n")
+    QTY = input ("Sounds good " + NAME.title() + ", how many cups of " + ORDER.title() + " would you like?\n")
     PRICE = 5.55
     print ("")
 elif ORDER.lower() == "cappucino":
-    QTY = input ("Sounds good " + NAME + ", how many cups of " + ORDER + " would you like?\n")
+    QTY = input ("Sounds good " + NAME.title() + ", how many cups of " + ORDER.title() + " would you like?\n")
     PRICE = 6.25
     print ("")
 else:
-    print ("Sorry " + NAME + " I didn't recognize your choice of " + ORDER)
+    print ("Sorry " + NAME.title() + " I didn't recognize your choice of " + ORDER.title())
     print ("Please try again")
     PRICE = 0  # set default value for PRICE
     exit(20)  # call exit as a function to exit the program
@@ -45,4 +45,4 @@ else:
 TOTAL = PRICE * int(QTY)
 
 # Use string formatting to display the total price with two decimal points
-print("Thanks " + NAME + ", that will be $%.2f please" % TOTAL)
+print("Thanks " + NAME.title() + ", that will be $%.2f please" % TOTAL)
