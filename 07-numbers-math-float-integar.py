828d2ab676df3079780747a082027912e2d74436
iastac84 <istacey@outlook.com>
2023-03-28 19:44:41 +0100

    100daysofpython

---
+++ 07-numbers-math-float-integar.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Day 7
+
+name = "Ian" 
+age = 21
+decpoint = 5.5
+
+print(name)
+print(age)
+print(decpoint)
+
+# View types, expecting str, int, float
+print(type(name))
+print(type(age))
+print(type(decpoint))
+print("")
+
+# Some math
+math = 6 ** 2 + 5 / 7 * 3 - 6 
+
+print(math)
+print(type(math))
+results = age + decpoint + math
+print(results)
+print(type(results))
+print("")
+
+# Division will always return float
+print("21 + 3")
+print(21 + 3)
+print(type(21 + 3))
+print("")
+print("21 * 3")
+print(21 * 3)
+print(type(21 * 3))
+print("")
+print("21 / 3")
+print(21 / 3)
+print(type(21 / 3))
+print("")
