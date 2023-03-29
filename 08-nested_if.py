0b5211434722fba7e18790cf73f3001dcd089f1e
iastac84 <istacey@outlook.com>
2023-03-29 22:08:07 +0100

    100daysofpython

---
+++ 08-nested_if.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Day 8
+
+# Nested if, if an action depends on a combination of conditions  
+country = input("What country do you live in?\n")
+tax = 0
+
+if country.lower() == 'canada':
+    province = input ("What province do you live in?\n")
+    if province in ('Alberta', 'Nunavut', 'Yukon'):
+        tax = 0.05
+    elif province == 'Ontario':
+        tax = 0.13
+    else:
+        tax = 0.15
+    print("For someone from " + province + " tax is calcuted at " + str (tax) + " %")
+    exit(15) 
+else:
+    tax = 0
+print("For someone from " + country + " tax is calcuted at " + str (tax) + " %")
