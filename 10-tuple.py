ae3c4367a7db3c76693a4652a11e8e3952db3ac9
iastac84 <istacey@outlook.com>
2023-04-03 16:43:16 +0100

    100daysofpython

---
+++ 10-tuple.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Day 10
+
+# Tuples are used to store multiple items in a single variable.
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+# A tuple is a collection which is ordered and unchangeable / immutable
+# Tuples are written with round brackets.
+
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+tuple4 = ("abc", 34, True, 40, "male")
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+print(tuple4)
+
+print(type(tuple1))
+
+# Tuple can be overwritten 
+tuple1 = ("chips", "burger", "carrot")
+print(tuple1)
+
