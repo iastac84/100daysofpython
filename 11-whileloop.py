7e6bfba1ce00bc0217d20eda22687f5fc1ec583d
iastac84 <istacey@outlook.com>
2023-04-05 16:10:09 +0100

    100daysofpython

---
+++ 11-whileloop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Day 11
+
+# While loop
+# With use of two built-in functions 
+# len() to get the total number of items in a list 
+# count() to count the occurrences of an element in a list
+
+players = ['Horvath','Osho','Lockyer','Bell','Drameh','Mpanzu','Nakamba','Doughty','Campbell','Morris','Adebayo']
+eleven = len(players)
+
+print ()
+print ("Luton " + str(eleven) + " team to face Watford")
+index = 0
+while index < len(players):
+    print(players[index])
+    index = index + 1
+
+print ()
+print ("C'mon you Hatters!!!!")
+print ()
+
+# A count of occurrences in the players list
+marvelous = players.count('Nakamba')
+print ("There's only " + str(marvelous) + " Marvelous " + (players[6]) + "!!! :)")
+
