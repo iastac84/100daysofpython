5a8ac0f9144b19b9c93f542a97064ca151f5d986
iastac84 <istacey@outlook.com>
2023-04-28 15:10:32 +0100

    100daysofpython

---
+++ 19-classes-objects-str-function.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Day 19
+
+"""
+The __str__() function controls what should be returned when the class object is represented as a string.
+If the __str__() function is not set, the string representation of the object is returned:
+"""
+
+print ("The string representation of an object WITHOUT the __str__() function:")
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1) 
+print()
+
+print ("The string representation of an object WITH the __str__() function:")
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1) 
