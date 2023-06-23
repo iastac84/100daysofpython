7c5467964471668028cfb8fe8e7daa66ccda8ec3
iastac84 <istacey@outlook.com>
2023-06-23 10:26:31 +0100

    100daysofpython

---
+++ 29-circle-radius.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Day 28
+
+'''
+Script to calculate the area of a circle by multiplying the square of the radius by the mathematical constant pi from the math module.
+'''
+
+import math
+
+# Prompt the user to enter the radius of the circle
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area of the circle
+area = math.pi * radius ** 2
+
+# Print the result
+print("The area of the circle is:", area)
+
