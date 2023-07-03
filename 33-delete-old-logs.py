550bc3695867c1b2afaed96d8372e7ee8c8f6b1c
iastac84 <istacey@outlook.com>
2023-07-03 20:57:05 +0100

    100daysofpython

---
+++ 33-delete-old-logs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Day 33
+
+'''
+Python script that achieves the task of deleting log entries for the previous day
+'''
+
+#!/usr/bin/env python3
+
+import os
+import datetime
+
+# Define the log directory path
+LOG_DIR = "/path/to/logs"
+
+# Get the date of the previous day
+previous_day = datetime.date.today() - datetime.timedelta(days=1)
+previous_day_str = previous_day.strftime("%Y-%m-%d")
+
+# Iterate over log files in the directory and delete entries for the previous day
+for root, dirs, files in os.walk(LOG_DIR):
+    for file in files:
+        if file.endswith(".log"):
+            file_path = os.path.join(root, file)
+            with open(file_path, "r+") as log_file:
+                lines = log_file.readlines()
+                log_file.seek(0)
+                log_file.truncate()
+                for line in lines:
+                    if previous_day_str not in line:
+                        log_file.write(line)
+
