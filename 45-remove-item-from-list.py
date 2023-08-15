56a30790154d8b90ef268b65935042c1dc2cdbfd
iastac84 <istacey@outlook.com>
2023-08-15 18:07:07 +0100

    100daysofpython

---
+++ 45-remove-item-from-list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Day 45
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+print("Check if banana is in the list")
+print("banana" in thislist)
+
+print("Now remove the banana")
+thislist.remove("banana")
+print("")
+print(thislist)
+print("Check if banana is in the list")
+print("banana" in thislist)
+
