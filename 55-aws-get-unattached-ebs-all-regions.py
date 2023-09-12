238a0475e34f7b86e8ce182adb8f7bea7328b0e5
iastac84 <istacey@outlook.com>
2023-09-12 16:31:56 +0100

    100daysofpython

---
+++ 55-aws-get-unattached-ebs-all-regions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Day 55
+
+"""
+Python script to list all unattached EBS volumes in an AWS account, all regions 
+Export/set AWS credentials before running
+Also set AWS_REGION and AWS_DEFAULT_REGION
+"""
+
+import boto3
+
+# Initialize the EC2 client
+ec2 = boto3.client('ec2')
+
+# Get a list of all regions
+all_regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
+
+# Loop through all regions
+for region in all_regions:
+    print(f"Region: {region}")
+
+    # Initialize a client for the current region
+    ec2 = boto3.client('ec2', region_name=region)
+
+    # Get all the volumes in the current region
+    volumes = ec2.describe_volumes()
+
+    # Filter for unattached volumes
+    unattached_volumes = [v for v in volumes['Volumes'] if v['State'] == 'available']
+
+    # Print the tags for each unattached volume
+    for volume in unattached_volumes:
+        if 'Tags' in volume:
+            tags = [f"{t['Key']}={t['Value']}" for t in volume['Tags']]
+            tag_string = ', '.join(tags)
+            print(f"Volume ID: {volume['VolumeId']} : Size (GiB): {volume['Size']} : AZ: {volume['AvailabilityZone']} : Type: {volume['VolumeType']} : Created: {volume['CreateTime']} : Tags: {tag_string}")
+
+        else:
+            print(f"Volume ID: {volume['VolumeId']} : Size (GiB): {volume['Size']} : AZ: {volume['AvailabilityZone']} : Type: {volume['VolumeType']} : Created: {volume['CreateTime']} : Tags: None")
+
