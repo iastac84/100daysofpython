c76bf3aa6522778e17042dd0b6a1297466697346
iastac84 <istacey@outlook.com>
2023-09-13 17:41:57 +0100

    100daysofpython

---
+++ 56-website-open.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Day 56
+
+'''
+'''
+
+import webbrowser
+
+# Prompt the user for a URL
+url = input("Enter a URL (including the full path, ie https://lutontown.co.uk: ")
+
+# Open the URL in the default web browser
+webbrowser.open(url)
+
