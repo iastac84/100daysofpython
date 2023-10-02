851b475eb6729080586a1301766543cec9b07aa8
iastac84 <istacey@outlook.com>
2023-10-02 17:26:47 +0100

    100daysofpython

--- 66-ppchallenge-capital-index.py
+++ 66-ppchallenge-capital-index.py
@@ -8,5 +8,11 @@ Capital indexes
 Write a function named capital_indexes. The function takes a single parameter, which is a string. Your function should return a list of all the indexes in the string that have capital letters.
 
 For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
+
+https://docs.python.org/3/library/functions.html#enumerate
 '''
 
+def capital_indexes(string):
+    return [i for i, char in enumerate(string) if char.isupper()]
+
+print(capital_indexes("HeLlO, Hello, hola, We're at a place called Vertigo")) 
