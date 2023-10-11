6ae6662bb58bc93739472fb8869aafe21a4af766
iastac84 <istacey@outlook.com>
2023-10-11 21:51:47 +0100

    100daysofpython

---
+++ 68-ppchallenge-dict.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Day 68
+
+# challenge from https://pythonprinciples.com/challenges
+'''
+Playing with dictionaries 
+'''
+
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+
+print(statuses)
+print ()
+print ("Here we use an object type of:")
+print (type(statuses))
+print ()
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict)
+print ()
+
+def myiterator():
+    for x in ():
+      print(x)
+
+myiterator(statuses)
+myiterator(thisdict)
