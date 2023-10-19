af2ac368932e266bc7f31279e07ebf9700b115d9
iastac84 <istacey@outlook.com>
2023-10-19 17:15:52 +0100

    100daysofpython

---
+++ 75-split.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Day 75
+
+# Splits
+# https://www.w3schools.com/python/ref_string_split.asp
+
+txt = "welcome to the jungle"
+x = txt.split()
+print(x)
+
+txt = "apple#banana#cherry#orange"
+x = txt.split("#")
+print(x)
+
+txt = "apple#banana#cherry#orange"
+# setting the maxsplit parameter to 1, will return a list with 2 elements!
+x = txt.split("#", 1)
+print(x)
+
+txt = "Luton.Town.F.C."
+x = txt.split(".", 2)
+print(x)
+
