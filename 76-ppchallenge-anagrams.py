1d869c5ca7cc89e8c990866bfacb6159a78ac17b
iastac84 <istacey@outlook.com>
2023-10-23 17:15:13 +0100

    100daysofpython

---
+++ 76-ppchallenge-anagrams.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Day 76
+
+# challenge from https://pythonprinciples.com/challenges
+'''
+Challenge
+Anagrams
+Two strings are anagrams if you can make one from the other by rearranging the letters.
+
+Write a function named is_anagram that takes two strings as its parameters. Your function should return True if the strings are anagrams, and False otherwise.
+
+For example, the call is_anagram("typhoon", "opython") should return True while the call is_anagram("Alice", "Bob") should return False.
+'''
+
