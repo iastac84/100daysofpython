9ded843f0970b027b4b7e9c9a7b54c9189092288
iastac84 <istacey@outlook.com>
2023-10-25 15:04:55 +0100

    100daysofpython

---
+++ 77-ppchallenge-flatten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Day 77
+
+# challenge from https://pythonprinciples.com/challenges
+'''
+Challenge
+Write a function that takes a list of lists and flattens it into a one-dimensional list.
+
+Name your function flatten. It should take a single parameter and return a list.
+
+For example, calling:
+
+flatten([[1, 2], [3, 4]])
+Should return the list:
+
+[1, 2, 3, 4]
+'''
+
+def flattenfunction(strg1, strg2):
+
+flattenfunction([[1, 2], [3, 4]])
