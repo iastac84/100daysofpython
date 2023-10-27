32bd5f2ce39dcc421bb0253dc0c16db461941a93
iastac84 <istacey@outlook.com>
2023-10-27 12:44:29 +0100

    100daysofpython

--- 79-ppchallenge-divby3-alt.py
+++ 79-ppchallenge-divby3-alt.py
@@ -8,7 +8,8 @@ Divisible by 3
 Define a function named div_3 that returns True if its single integer parameter is divisible by 3 and False otherwise.
 For example, div_3(6) is True because 6/3 does not leave any remainder. However div_3(5) is False because 5/3 leaves 2 as a remainder.
 
-Alt use the modulo operator %
+Alternatively use %, the % symbol in Python is called the Modulo Operator. 
+It returns the remainder of dividing the left hand operand by right hand operand. It's used to get the remainder of a division problem.
 '''
 
 def div_3(x):
