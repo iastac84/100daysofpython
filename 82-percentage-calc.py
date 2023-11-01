1e1db03bf72f55d70687940ce2bde0789d76b226
iastac84 <istacey@outlook.com>
2023-11-01 17:12:32 +0000

    100daysofpython

---
+++ 82-percentage-calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Day 82
+
+def calculate_percentage(num1, num2):
+    return (num1 / num2) * 100
+
+print(calculate_percentage(25, 50))  # Output: 50.0
+print(calculate_percentage(1342.49, 3351.34))  # Output: 50.0
+print(calculate_percentage(333.18, 3351.34))  # Output: 50.0
