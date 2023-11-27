870e9c0ea443e693c8b1f0b57fcd54f8a24ec478
iastac84 <istacey@outlook.com>
2023-11-27 15:28:48 +0000

    100daysofpython

--- 94-ppchallenge-list-xor.py
+++ 94-ppchallenge-list-xor.py
@@ -16,3 +16,19 @@ list_xor(1, [0, 2, 3], [1, 5, 6]) == True
 list_xor(1, [1, 2, 3], [1, 5, 6]) == False
 list_xor(1, [0, 0, 0], [4, 5, 6]) == False
 '''
+
+def list_xor(n, list1, list2):
+
+    list_one = n in list1
+    list_tow = n in list2
+
+    if list_one != list_tow:
+        return True
+
+    if list_one == list_tow:
+        return False
+
+print(list_xor(1, [1, 2, 3], [4, 5, 6]))
+print(list_xor(2, [1, 2, 3], [4, 5, 6]))
+print(list_xor(1, [1, 2, 3], [1, 5, 6]))
+print(list_xor(1, [0, 0, 0], [4, 5, 6]))
