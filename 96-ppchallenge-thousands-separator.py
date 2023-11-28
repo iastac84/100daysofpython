18b0b1525853c0da3c5511741bcb8de0bcddf473
iastac84 <istacey@outlook.com>
2023-11-28 16:17:50 +0000

    100daysofpython

---
+++ 96-ppchallenge-thousands-separator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Day 96
+
+# challenge from https://pythonprinciples.com/challenges
+# last of these
+'''
+Challenge
+Thousands separator
+Write a function named format_number that takes a non-negative number as its only parameter.
+Your function should convert the number to a string and add commas as a thousands separator.
+For example, calling format_number(1000000) should return "1,000,000".
+
+Hint: 
+Convert the number to a string and iterate over each digit. Iterate backwards, or start by reversing the string, to make your life easier.
+'''
